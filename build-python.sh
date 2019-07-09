a2e9cd6e29cbea06449f0d337e86451881b2ed99
Doug Cox <doug@lumen5.com>
2019-07-09 16:29:01 -0700

    fix

--- build-python.sh
+++ build-python.sh
@@ -4,9 +4,13 @@ npm install
 
 node build/swagger/openapiToSwagger.js
 
-mkdir -p dist
+mkdir -p shutterstock_dist
 
 docker run --rm \
 	   -v $(pwd):/opt/node-sdk \
 	   -w /opt/node-sdk \
-	   swaggerapi/swagger-codegen-cli:2.4.2 generate -i ./swagger2.json -o ./dist -c ./build/sdk/py/config.json -l python
+	   swaggerapi/swagger-codegen-cli:2.4.2 generate -i ./swagger2.json -o ./shutterstock_dist -c ./build/sdk/py/config.json -l python
+
+pushd shutterstock_dist
+git checkout -- git_push.sh
+popd
