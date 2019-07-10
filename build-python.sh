2ff78dbe4cd734e2f0f225e1cf778cf047c16e51
Doug Cox <doug@lumen5.com>
2019-07-10 15:44:46 -0700

    Added custom .gitignore template.

--- build-python.sh
+++ build-python.sh
@@ -9,7 +9,7 @@ mkdir -p shutterstock_dist
 docker run --rm \
 	   -v $(pwd):/opt/node-sdk \
 	   -w /opt/node-sdk \
-	   swaggerapi/swagger-codegen-cli:2.4.2 generate -i ./swagger2.json -o ./shutterstock_dist -c ./build/sdk/py/config.json -l python
+	   swaggerapi/swagger-codegen-cli:2.4.2 generate -i ./swagger2.json -o ./shutterstock_dist -c ./build/sdk/py/config.json -l python -t ./build/sdk/py/templates
 
 pushd shutterstock_dist
 git checkout -- git_push.sh
