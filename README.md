bfa103d4777b32d4af61fb696ca8689754204d3b
Doug Cox <doug@lumen5.com>
2021-01-26 09:49:45 -0800

    Remove references to Shutterstock

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Generate Public API SDKs
 
 - Generates Python SDK with swagger-codegen upon commit to this repo
-- Pushes SDK to [Lumen5/shutterstock-api](https://github.com/Lumen5/shutterstock-api)
+- Pushes SDK to [Lumen5/storyblocks-api](https://github.com/Lumen5/storyblocks-api)
--- build-python.sh
+++ build-python.sh
@@ -4,13 +4,13 @@ npm install
 
 node build/swagger/openapiToSwagger.js
 
-mkdir -p shutterstock_dist
+mkdir -p storyblocks_dist
 
 docker run --rm \
 	   -v $(pwd):/opt/node-sdk \
 	   -w /opt/node-sdk \
-	   swaggerapi/swagger-codegen-cli:2.4.17 generate -i ./swagger2.json -o ./shutterstock_dist -c ./build/sdk/py/config.json -l python -t ./build/sdk/py/templates
+	   swaggerapi/swagger-codegen-cli:2.4.17 generate -i ./swagger2.json -o ./storyblocks_dist -c ./build/sdk/py/config.json -l python -t ./build/sdk/py/templates
 
-pushd shutterstock_dist
+pushd storyblocks_dist
 git checkout -- git_push.sh
 popd
